d66b9e9ce5216e5486aec2d09eada981ae21c6cb
eJunior08 <elsonfojr@gmail.com>
2020-07-10 02:43:07 -0300

    Components de UserList e ChannelData inseridos no Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
 import { Grid } from "./styles";
 
-import ServerList from '../ServerList';
-import ServerName from '../ServerName';
-import ChannelInfo from '../ChannelInfo';
-import ChannelList from '../ChannelList';
-import UserInfo from '../UserInfo';
+import ServerList from "../ServerList";
+import ServerName from "../ServerName";
+import ChannelInfo from "../ChannelInfo";
+import ChannelList from "../ChannelList";
+import UserInfo from "../UserInfo";
+import UserList from "../UserList";
+import ChannelData from "../ChannelData";
 
 const Layout: React.FC = () => {
   return (
@@ -16,8 +18,10 @@ const Layout: React.FC = () => {
       <ChannelInfo />
       <ChannelList />
       <UserInfo />
+      <ChannelData />
+      <UserList />
     </Grid>
   );
-}
+};
 
 export default Layout;
